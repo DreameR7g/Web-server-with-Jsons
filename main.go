b1ffd466e2219f887a6a17aa17dea3f84ca72b84
Andrew Kernats <andrew_kernats@comcast.net>
2022-03-02 13:20:19 -0600

    add additional handler and put payload of json on handler

--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
-
-type likesStuff struct {
-	Name string `json:"name"`
-
-	Likes string `json:"likes"`
-
-	Dislikes string `json:"dislikes"`
-
-	Skills string `json:"skills"`
-}
 
 func jsonHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/json" {
@@ -27,6 +19,38 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var lotsofStuff string
+
+	stuff, err := os.ReadFile("./jsons/stuff.json")
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	_ = json.Unmarshal(stuff, &lotsofStuff)
+
+	w.Write(stuff)
+
+}
+
+func boopHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/boop" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	var tonsofBoops string
+
+	boop, err := os.ReadFile("./jsons/boop.json")
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	_ = json.Unmarshal(boop, &tonsofBoops)
+
+	w.Write(boop)
 }
 
 func main() {
@@ -34,6 +58,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/json", jsonHandler)
+	http.HandleFunc("/boop", boopHandler)
 
 	fmt.Printf("Starting server at port 1338\n")
 	if err := http.ListenAndServe(":1338", nil); err != nil {
